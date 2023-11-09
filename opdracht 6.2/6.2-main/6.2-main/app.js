6a651db5c823977255c4f3bf6ab717bec63ad752
Koen-Radenborg <143798312+Koen-Radenborg@users.noreply.github.com>
2023-11-09 12:51:36 +0100

    Add files via upload

---
+++ opdracht 6.2/6.2-main/6.2-main/app.js
@@ -0,0 +1,5 @@
+function changecolor(){
+    document.getElementById("The One Piece").innerHTML = "The One Piece is real!!"
+    document.getElementById("The One Piece").style.color = "blue"
+    console.log("The One Piece is real!!")
+}
