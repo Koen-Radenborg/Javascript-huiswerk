6c801060f6cd4097f51c96d7c0c7c04bfd89a5d5
Koen-Radenborg <143798312+Koen-Radenborg@users.noreply.github.com>
2023-11-09 13:09:02 +0100

    Add files via upload

---
+++ opdracht 6.2/opdracht 6.4/app.js
@@ -0,0 +1,25 @@
+function calculator(num, num2) {
+    let answer = num + num2; 
+    console.log(answer)
+}
+
+calculator(8,15);
+
+//maak een function genaamd multiply
+//de parameters verwachten 1 waarden
+//IN de functie die je die waarde keer 5
+
+
+function MultipleByFive(num) {
+    let answer = num * 5; 
+    console.log(answer)
+}
+
+MultipleByFive(2393);
+
+function calculateMinutes(num) {
+    let answer = num / 60; 
+    console.log(answer)
+}
+
+calculateMinutes(3600)
